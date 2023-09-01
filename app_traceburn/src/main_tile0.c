cd302f94794a8a3881475a3f31e468722b6d7c2a
Angel Cascarino <angelcascarino@xmos.com>
2023-09-01 16:48:52 +0100

    Get tracing working

--- app_traceburn/src/main_tile0.c
+++ app_traceburn/src/main_tile0.c
@@ -5,12 +5,16 @@
 #include "debug_print.h"
 #include "rtos_printf.h"
 
-#include "tile_common.h"
-#include "platform/platform_init.h"
-
+#include "platform.h"
 #include <xs1.h>
 #include <xcore/chanend.h>
 #include <xcore/parallel.h>
+
+#include "FreeRTOS.h"
+#include "tile_common.h"
+#include "platform/platform_init.h"
+
+#if ON_TILE(0)
 
 static void rtos_app(void* args)
 {
@@ -21,5 +25,10 @@ static void rtos_app(void* args)
 
 void main_tile0(chanend_t c0, chanend_t c1, chanend_t c2, chanend_t c3)
 {
+#if (USE_TRACE_MODE == TRACE_MODE_TRACEALYZER_STREAMING)
+    xTraceInitialize();
+    xTraceEnable(TRC_START);
+#endif
     tile_common_start_rtos_app(c1, rtos_app, RTOS_THREAD_STACK_SIZE(rtos_app), NULL);
 }
+#endif
--- app_traceburn/src/main_tile1.c
+++ app_traceburn/src/main_tile1.c
@@ -5,12 +5,16 @@
 #include "debug_print.h"
 #include "rtos_printf.h"
 
-#include "tile_common.h"
-#include "platform/platform_init.h"
-
+#include "platform.h"
 #include <xs1.h>
 #include <xcore/chanend.h>
 #include <xcore/parallel.h>
+
+#include "FreeRTOS.h"
+#include "tile_common.h"
+#include "platform/platform_init.h"
+
+#if ON_TILE(1)
 
 static void rtos_app(void* args)
 {
@@ -23,3 +27,4 @@ void main_tile1(chanend_t c0, chanend_t c1, chanend_t c2, chanend_t c3)
 {
     tile_common_start_rtos_app(c0, rtos_app, RTOS_THREAD_STACK_SIZE(rtos_app), NULL);
 }
+#endif
