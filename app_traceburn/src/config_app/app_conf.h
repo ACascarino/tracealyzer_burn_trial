d1bd7a26a5e745359b2cded7ca98ab8e81bfbbcc
Angel Cascarino <angelcascarino@xmos.com>
2023-10-31 16:17:40 +0000

    Break applicaiton

--- app_traceburn/src/config_app/app_conf.h
+++
@@ -1,5 +0,0 @@
-#define appconfSTARTUP_TASK_PRIORITY              (configMAX_PRIORITIES - 2)
-#define appconfI2S_ENABLED                        1
-#define appconfI2S_AUDIO_SAMPLE_RATE              16000
-#define appconfAUDIO_PIPELINE_SAMPLE_RATE         16000
-#define appconfAUDIO_PIPELINE_FRAME_ADVANCE       256
--- app_traceburn/src/main_tile1.c
+++ app_traceburn/src/main_tile1.c
@@ -1,30 +1,12 @@
-#define DEBUG_UNIT MAIN_TILE1
-#ifndef DEBUG_PRINT_ENABLE_MAIN_TILE1
-#define DEBUG_PRINT_ENABLE_MAIN_TILE1 1
-#endif
-#include "debug_print.h"
-#include "rtos_printf.h"
-
 #include "platform.h"
 #include <xs1.h>
-#include <xcore/chanend.h>
-#include <xcore/parallel.h>
 
 #include "FreeRTOS.h"
-#include "tile_common.h"
-#include "platform/platform_init.h"
 
 #if ON_TILE(1)
 
-static void rtos_app(void *args)
-{
-    debug_printf("RTOS starting on tile[%d]\n", THIS_XCORE_TILE);
-    // platform_start();
-    vTaskDelete(NULL);
-}
-
 void main_tile1(chanend_t c0, chanend_t c1, chanend_t c2, chanend_t c3)
 {
-    tile_common_start_rtos_app(c0, rtos_app, RTOS_THREAD_STACK_SIZE(rtos_app), NULL);
+    return;
 }
 #endif
